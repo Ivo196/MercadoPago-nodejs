bc033755bdd865cee80aca9b90af617dc8c7e499
Ivo196 <ivo.tonioni@gmail.com>
2023-05-26 17:59:56 +0200

    first commit

---
+++ src/controllers/payment.controllers.js
@@ -0,0 +1,40 @@
+import mercadopago from "mercadopago";
+import { HOST, MERCADOPAGO_API_KEY } from "../config.js";
+
+export const createOrder = async (req, res) => {
+  mercadopago.configure({
+    access_token : MERCADOPAGO_API_KEY
+  });
+  const result = await mercadopago.preferences.create({
+    items: [
+      {
+        title: "Laptop",
+        unit_price: 500,
+        currency_id: "ARS",
+        quantity: 1,
+      },
+    ],
+    back_urls: {
+      success: `${HOST}/success`,
+      failure: `${HOST}/failure`,
+      pending: `${HOST}/pending`,
+    },
+    notification_url: "https://f73c-85-80-253-192.eu.ngrok.io/webhook",
+  });
+  console.log(result);
+  res.send(result.body);
+};
+
+export const receiveWebHook = async (req, res) => {
+  const payment = req.query;
+  try {
+    if (payment.type === "payment") {
+      const data = await mercadopago.payment.findById(payment["data.id"]);
+      console.log(data);
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500).json({ error: error.message });
+  }
+};
